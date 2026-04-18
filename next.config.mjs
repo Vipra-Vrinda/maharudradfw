a29fde6026ceef990c56715ef1c235aef54824d4
Aaditya Murthy <aaditya.murthy@toyotaconnected.com>
2026-04-17 19:43:06 -0500

    clean up maharudra page

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-    basePath: "/maharudradfw",
-    assetPrefix: "/maharudradfw",
-};
-
-export default nextConfig;
