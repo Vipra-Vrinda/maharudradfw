12c57777c370ac2ef2c494b6107e1deb67a467e6
Aaditya Murthy <aaditya.murthy@toyotaconnected.com>
2026-04-17 19:54:16 -0500

    Revert "clean up maharudra page"
    This reverts commit a29fde6026ceef990c56715ef1c235aef54824d4.

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    basePath: "/maharudradfw",
+    assetPrefix: "/maharudradfw",
+};
+
+export default nextConfig;
